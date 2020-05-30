f177a0913d2db88220e448f80343f796feef1e05
leghata35 <63017445+leghata35@users.noreply.github.com>
2020-05-30 22:26:32 +0200

    Add files via upload

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if(request('q') !== null){
+            $tasks['data'] = Task::where('name', 'like', '%' . request('q') . '%')->get();
+            return response()->json($tasks);
+        }
+        return $this->refresh();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $created = Task::create($request->all());
+        if($created){
+            return $this->refresh();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $task = Task::find($id);
+        return response()->json($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id)
+    {
+        $task = Task::find($id);
+        $task->name = request('name');
+        $task->save();
+        if($task){
+            return $this->refresh();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        if($task->delete()){
+            return $this->refresh();
+        }else{
+            return response()->json(['error' => 'Delete method has failed'], 425);
+        }
+    }
+
+    public function refresh(){
+        $tasks = Task::orderBy('created_at', 'desc')->paginate(3);
+        return response()->json($tasks);
+    }
+}
